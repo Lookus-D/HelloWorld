675d127c2f7eb23ca1ecffdef67d452ca8b7f1aa
User <User@DESKTOP-ROOTJT0>
2023-03-27 15:53:21 +0200

    Add project files.

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string imie = "Łukasz";
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Nazywam się Łukasz Dudek");
+        }
+    }
+}
+
+
+
